ad7eb8e7dc56d3cee75e7ce4864ccc45b66a73d1
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-02-17 16:13:38 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/camera/MainActivity.java
@@ -0,0 +1,46 @@
+package com.gzeinnumer.camera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn)
+    Button btn;
+    @BindView(R.id.image)
+    ImageView image;
+
+    private static final int CAMERA_REQUEST=123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn)
+    public void onViewClicked() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode ==CAMERA_REQUEST && resultCode == Activity.RESULT_OK){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(bitmap);
+        }
+    }
+}
